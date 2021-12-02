613108eaa784410b1bebe0cb001e215f2ccc679c
mikail-ozdemir <professional.mikailozdemir@gmail.com>
2021-12-02 19:44:27 +0100

    implementation de systeme de auth ainsi que mise en place de condition

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/foo', 'App\Http\Controllers\TestController@foo');
+Route::get('/bar', 'App\Http\Controllers\TestController@bar');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
